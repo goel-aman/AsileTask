e5f923ac444150c31a4b40c2ee03e25bdf482cd0
Aman Goel <amangoel9873572693@gmail.com>
2024-11-09 14:24:10 +0530

    Adding keychain functionality.

---
+++ AsileTask/AsileTask/KeychainManager.swift
@@ -0,0 +1,25 @@
+//
+//  KeychainManager.swift
+//  AsileTask
+//
+//  Created by aman on 09/11/24.
+//
+
+import Foundation
+
+class KeychainManager {
+    
+    enum KeychainError: Error {
+        case duplicateEntry
+        case unknown(OSStatus)
+    }
+    
+    static func save() throws {
+        
+    }
+    
+    
+    static func get() {
+        
+    }
+}
